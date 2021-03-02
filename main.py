5a056aedf2bbfab1cb4fbc624e3e2c37b5c70239
W7k-J <59022102+W7k-J@users.noreply.github.com>
2021-03-02 18:39:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,26 +7,7 @@ Created on Thu Feb 18 11:21:52 2021
 @https://github.com/W7k-J
 """
 
-
 import random
-
-
-print("H A N G M A N")
-dictionary = ['python', 'java', 'kotlin', 'javascript']
-word = random.choice(dictionary)
-
-# word_hidden = word[:3] + ("-") * (len(word)-3)
-
-# guess = input(f"Guess the word {word_hidden}: ")
-
-# if guess == word:
-#     print("You survived!")
-# else:
-#     print("You lost!")
-
-# print('The game will be available soon.')
-
-# print(word)
 
 def char_changer(word, word_hidden, char):
     temp_list = list(word_hidden) 
@@ -35,23 +16,57 @@ def char_changer(word, word_hidden, char):
             temp_list[idk] = char
     return "".join(temp_list)
 
- 
-
-
-word_hidden = "-" * len(word)
-total = 0
-
-
-while total < 8:
-    total += 1
-    # print(total)
-    char = input("Input a letter:")
-    if char not in word:
-        print("That letter doesn't appear in the word")
-    else:
-        word_hidden = char_changer(word, word_hidden, char)
+def game():    
+    dictionary = ['python', 'java', 'kotlin', 'javascript']
+    word = random.choice(dictionary)
+    word_hidden = "-" * len(word)
+    total = 0
+    letter_tried = []
+    
+    while True:
         print(word_hidden)
+        char = input("Input a letter: ")
+        if len(char) != 1:
+            print("You should input a single letter")
+            print("")
+            continue
+        if not char.islower():
+            print("Please enter a lowercase English letter")
+            print("")
+            continue
+        if char in letter_tried:
+            print("You've already guessed this letter")
+            print("")
+            continue
+        
+    
+        letter_tried.append(char)
+        
+        if char in word:
+            word_hidden = char_changer(word, word_hidden, char)
+        else:
+            total += 1
+            print("That letter doesn't appear in the word")
+        
+    
+        if word_hidden == word:
+            print("You guessed the word {word}!")
+            print("You survived!")
+            break
+        if total == 8:
+            print("You lost!")
+            break
+        print("")
+        
     
     
-print("Thanks for playing\"!")
-print("We'll see how well you did in the next stage")
+print("H A N G M A N")
+while True:
+    pytanie = input('Type "play" to play the game, "exit" to quit: ')
+    if pytanie == "play":
+        print("")
+        game()
+    else:
+        break
+
+
