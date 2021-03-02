8d6e2599cef9c183c58f94e8a62143791dbf0da3
W7k-J <59022102+W7k-J@users.noreply.github.com>
2021-03-02 16:11:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,54 @@
 Created on Thu Feb 18 11:21:52 2021
 
 @author: w7k-j
+@https://github.com/W7k-J
 """
 
-# Write your code here
-print("H A N G M A N")
-guess = input("Guess the word: ")
-if guess == "python":
-    print("You survived!")
-else:
-    print("You lost!")
 
-print('The game will be available soon.')
+import random
+
+
+print("H A N G M A N")
+dictionary = ['python', 'java', 'kotlin', 'javascript']
+word = random.choice(dictionary)
+
+# word_hidden = word[:3] + ("-") * (len(word)-3)
+
+# guess = input(f"Guess the word {word_hidden}: ")
+
+# if guess == word:
+#     print("You survived!")
+# else:
+#     print("You lost!")
+
+# print('The game will be available soon.')
+
+# print(word)
+
+def char_changer(word, word_hidden, char):
+    temp_list = list(word_hidden) 
+    for idk, k in enumerate(word):
+        if k == char:
+            temp_list[idk] = char
+    return "".join(temp_list)
+
+ 
+
+
+word_hidden = "-" * len(word)
+total = 0
+
+
+while total < 8:
+    total += 1
+    # print(total)
+    char = input("Input a letter:")
+    if char not in word:
+        print("That letter doesn't appear in the word")
+    else:
+        word_hidden = char_changer(word, word_hidden, char)
+        print(word_hidden)
+    
+    
+print("Thanks for playing\"!")
+print("We'll see how well you did in the next stage")
