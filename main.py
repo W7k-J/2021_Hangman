05943357dcbf43e7d602f471287b545ae9ca3f29
W7k-J <59022102+W7k-J@users.noreply.github.com>
2021-03-02 07:23:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Thu Feb 18 11:21:52 2021
+
+@author: w7k-j
+"""
+
+# Write your code here
+print("H A N G M A N")
+guess = input("Guess the word: ")
+if guess == "python":
+    print("You survived!")
+else:
+    print("You lost!")
+
+print('The game will be available soon.')
